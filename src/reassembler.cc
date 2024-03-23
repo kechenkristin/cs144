fcac2a98159edf368535e83a39303cca742c7ac9
kechenkristin <kechenkristin@gmail.com>
2024-03-23 23:16:27 +0000

    c1 third try

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -20,32 +20,30 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   }
 
   uint64_t data_index{0}; // The index of start of valid data, only apply to the passed string param
-  uint64_t actual_index(first_index); // The actual start index of the string in the axis(initialize as first_index in case 1, 2, 4; equals to _next_index in case 3)
+  uint64_t actual_index{first_index}; // The actual start index of the string in the axis(initialize as first_index in case 1, 2, 4; equals to _next_index in case 3)
 
 
-  uint64_t start_index = _next_index % _capacity;
+
   // case 3
-//  if (first_index < _next_index) {
-//    actual_index = _next_index;
-//    data_index = _next_index - first_index;
-//  }
-
-  if (first_index < start_index) {
-    actual_index = start_index;
-    data_index = start_index - first_index;
+  if (first_index < _next_index) {
+    actual_index = _next_index;
+    data_index += _next_index - first_index;
   }
 
-  uint64_t first_unacceptable_index = _next_index + _capacity + reader().bytes_buffered();
+//  if (first_index < start_index) {
+//    actual_index = start_index;
+//    data_index = start_index - first_index;
+//  }
+
+  uint64_t start_index = _next_index % _capacity;
+  uint64_t loop_index = actual_index % _capacity;
+  uint64_t first_unacceptable_index = _next_index + _capacity - reader().bytes_buffered();
   if (first_unacceptable_index <= actual_index) {
     return;
   }
 
   uint64_t loop_size = min(data.size() - data_index, first_unacceptable_index - actual_index);
 
-  // Here we need to find the start index and loop index
-  // this is the start index of the `_stream`, which is the `_next_index` nothing change just different name
-  // this is the loop index of the `_stream`, which is the index where we should start to store the data,
-  uint64_t loop_index = actual_index % _capacity;
   for (uint64_t i = loop_index, j = data_index; j < data_index + loop_size; i = next(i), j++) {
     if (!_wait_map.contains(i)) {
       _wait_map.insert( make_pair(i, data[j]));
--- tests/reassembler_cap.cc
+++ tests/reassembler_cap.cc
@@ -8,46 +8,46 @@ using namespace std;
 int main()
 {
   try {
-//    {
-//      ReassemblerTestHarness test { "all within capacity", 2 };
-//
-//      test.execute( Insert { "ab", 0 } );
-//      test.execute( BytesPushed( 2 ) );
-//      test.execute( BytesPending( 0 ) );
-//      test.execute( ReadAll( "ab" ) );
-//
-//      test.execute( Insert { "cd", 2 } );
-//      test.execute( BytesPushed( 4 ) );
-//      test.execute( BytesPending( 0 ) );
-//      test.execute( ReadAll( "cd" ) );
-//
-//      test.execute( Insert { "ef", 4 } );
-//      test.execute( BytesPushed( 6 ) );
-//      test.execute( BytesPending( 0 ) );
-//      test.execute( ReadAll( "ef" ) );
-//    }
-//
-//    {
-//      ReassemblerTestHarness test { "insert beyond capacity", 2 };
-//
-//      test.execute( Insert { "ab", 0 } );
-//      test.execute( BytesPushed( 2 ) );
-//      test.execute( BytesPending( 0 ) );
-//
-//      test.execute( Insert { "cd", 2 } );
-//      test.execute( BytesPushed( 2 ) );
-//      test.execute( BytesPending( 0 ) );
-//
-//      test.execute( ReadAll( "ab" ) );
-//      test.execute( BytesPushed( 2 ) );
-//      test.execute( BytesPending( 0 ) );
-//
-//      test.execute( Insert { "cd", 2 } );
-//      test.execute( BytesPushed( 4 ) );
-//      test.execute( BytesPending( 0 ) );
-//
-//      test.execute( ReadAll( "cd" ) );
-//    }
+    {
+      ReassemblerTestHarness test { "all within capacity", 2 };
+
+      test.execute( Insert { "ab", 0 } );
+      test.execute( BytesPushed( 2 ) );
+      test.execute( BytesPending( 0 ) );
+      test.execute( ReadAll( "ab" ) );
+
+      test.execute( Insert { "cd", 2 } );
+      test.execute( BytesPushed( 4 ) );
+      test.execute( BytesPending( 0 ) );
+      test.execute( ReadAll( "cd" ) );
+
+      test.execute( Insert { "ef", 4 } );
+      test.execute( BytesPushed( 6 ) );
+      test.execute( BytesPending( 0 ) );
+      test.execute( ReadAll( "ef" ) );
+    }
+
+    {
+      ReassemblerTestHarness test { "insert beyond capacity", 2 };
+
+      test.execute( Insert { "ab", 0 } );
+      test.execute( BytesPushed( 2 ) );
+      test.execute( BytesPending( 0 ) );
+
+      test.execute( Insert { "cd", 2 } );
+      test.execute( BytesPushed( 2 ) );
+      test.execute( BytesPending( 0 ) );
+
+      test.execute( ReadAll( "ab" ) );
+      test.execute( BytesPushed( 2 ) );
+      test.execute( BytesPending( 0 ) );
+
+      test.execute( Insert { "cd", 2 } );
+      test.execute( BytesPushed( 4 ) );
+      test.execute( BytesPending( 0 ) );
+
+      test.execute( ReadAll( "cd" ) );
+    }
 
     {
       ReassemblerTestHarness test { "overlapping inserts", 1 };
